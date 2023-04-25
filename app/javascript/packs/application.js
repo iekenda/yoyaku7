e3429829143deb44f04759ce652efae49e061fa7
家村健太郎 <aho@iemurakentarounoMacBook-Air.local>
2023-04-25 12:57:04 +0900

    ダッシュボード

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,18 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+window.Noty = require("noty")
+
+$(document).ready(() => {
+  $('.toggle').on('click', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      $('#' + e.target.getAttribute('aria-controls')).toggleClass('is-hidden');
+  })
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-
-  devise_for :users
+  devise_for :users, 
+              path: '', 
+              path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
+              controllers: {registrations: 'registrations'}
   get 'pages/home'
+  get '/dashboard', to: 'users#dashboard'
+  
+  post '/users/edit', to: 'users#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
